6774abce4bf5ffc060243efd56c24349ac05905d
GTanWan <142838774+GTanWan@users.noreply.github.com>
2024-11-24 23:28:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -213,5 +213,5 @@ def powerbi_dashboard():
     </div>
     """
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
